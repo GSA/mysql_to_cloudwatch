edee7f8d9262a43894e85f470b19b5e75754f51b
Aidan Feldman <aidan.feldman@gsa.gov>
2017-03-26 23:16:10 -0400

    have script list the tables

--- mysql_to_cloudwatch.py
+++ mysql_to_cloudwatch.py
@@ -1,3 +1,12 @@
 import boto3
+import MySQLdb
 
 print("started")
+
+db = MySQLdb.connect(host="mysql", db="mysql")
+c = db.cursor()
+
+c.execute("SHOW TABLES")
+tables = c.fetchall()
+for (table_name,) in tables:
+    print(table_name)
