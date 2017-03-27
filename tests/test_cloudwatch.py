1345c965f596c1c7c5f24f278db77cf49e49181b
Aidan Feldman <aidan.feldman@gsa.gov>
2017-03-27 14:50:34 -0400

    stub the cloudwatch-focused tests

---
+++ tests/test_cloudwatch.py
@@ -0,0 +1,15 @@
+# TODO test_create_log_group_present
+
+# TODO test_create_log_group_empty
+
+# TODO test_get_log_stream_present
+
+# TODO test_get_log_stream_empty
+
+# TODO test_create_log_stream_present
+
+# TODO test_create_log_stream_empty
+
+# TODO test_upload_logs
+
+# TODO test_upload_logs_without_seq_token
