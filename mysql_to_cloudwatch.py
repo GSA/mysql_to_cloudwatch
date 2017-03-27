13af35806d6ca4806ccd6aeebd7ef1c342cf85e7
Aidan Feldman <aidan.feldman@gsa.gov>
2017-03-27 01:20:07 -0400

    fetch entries from the general log

--- mysql_to_cloudwatch.py
+++ mysql_to_cloudwatch.py
@@ -6,7 +6,19 @@ print("started")
 db = MySQLdb.connect(host="mysql", db="mysql")
 c = db.cursor()
 
+# TODO fetch error log
+
+c.execute("SET GLOBAL log_output = 'TABLE'")
+c.execute("SET GLOBAL general_log = 'ON'")
+db.commit()
+
 c.execute("SHOW TABLES")
 tables = c.fetchall()
 for (table_name,) in tables:
     print(table_name)
+
+# TODO select since last time
+c.execute("SELECT * FROM general_log")
+rows = c.fetchall()
+for row in rows:
+    print(row)
