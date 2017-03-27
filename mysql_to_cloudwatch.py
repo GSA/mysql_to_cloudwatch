c094785f75c289f56fc24f5c63929504ee8dcecf
Aidan Feldman <aidan.feldman@gsa.gov>
2017-03-27 15:56:11 -0400

    remove hard-coding of database connection information

--- mysql_to_cloudwatch.py
+++ mysql_to_cloudwatch.py
@@ -1,13 +1,23 @@
 import boto3
 # http://mysqlclient.readthedocs.io/en/latest/user_guide.html#mysqldb
 import MySQLdb
+import os
 from src import cloudwatch
 from src import mysql
 
 
-DB_HOST = "mysql"
-LOG_GROUP_NAME = "mysql_to_cloudwatch-test"
-LOG_STREAM_NAME = DB_HOST
+DB_HOST = os.environ['DB_HOST']
+db_kwargs = {
+    'host': DB_HOST,
+    'db': 'mysql'
+}
+for key in ['user', 'passwd', 'port']:
+    env_var = "DB_{}".format(key.upper())
+    if env_var in os.environ:
+        db_kwargs[key] = os.environ[env_var]
+
+LOG_GROUP_NAME = os.environ['LOG_GROUP_NAME']
+LOG_STREAM_NAME = os.environ.get('LOG_STREAM_NAME', DB_HOST)
 
 
 def test_setup(db, cw_client, group, stream):
@@ -21,7 +31,7 @@ def copy_general_logs(db, cw_client, group, stream, since, seq_token=None):
 
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host=DB_HOST, db="mysql")
+    db = MySQLdb.connect(**db_kwargs)
     cw_client = boto3.client('logs')
 
     test_setup(db, cw_client, LOG_GROUP_NAME, LOG_STREAM_NAME)
