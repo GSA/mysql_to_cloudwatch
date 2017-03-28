f7da4b26c4c8e0fe0621f42670b8609c9086317e
Aidan Feldman <aidan.feldman@gsa.gov>
2017-03-28 03:20:59 -0400

    create a run() function

--- run.py
+++ run.py
@@ -24,17 +24,19 @@ def copy_general_logs(db, cw_client, group, stream, since, seq_token=None):
     events = mysql.get_general_log_events(db, since)
     cloudwatch.upload_logs(cw_client, group, stream, events, seq_token=seq_token)
 
+def run(db, cw_client, group, stream):
+    since = cloudwatch.get_latest_cw_event(cw_client, group, stream)
+    seq_token = cloudwatch.get_seq_token(cw_client, group, stream)
+
+    # TODO copy error log
+    copy_general_logs(db, cw_client, group, stream, since, seq_token=seq_token)
+
 
 if __name__ == '__main__':
     db = pymysql.connect(**DB_KWARGS)
     cw_client = boto3.client('logs')
 
     test_setup(db, cw_client, LOG_GROUP_NAME, LOG_STREAM_NAME)
-
-    since = cloudwatch.get_latest_cw_event(cw_client, LOG_GROUP_NAME, LOG_STREAM_NAME)
-    seq_token = cloudwatch.get_seq_token(cw_client, LOG_GROUP_NAME, LOG_STREAM_NAME)
-
-    # TODO copy error log
-    copy_general_logs(db, cw_client, LOG_GROUP_NAME, LOG_STREAM_NAME, since, seq_token=seq_token)
+    run(db, cw_client, LOG_GROUP_NAME, LOG_STREAM_NAME)
 
     print("DONE")
