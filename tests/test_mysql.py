a361697dbce843e797a5d9a048f05523feae6f71
Aidan Feldman <aidan.feldman@gsa.gov>
2017-04-05 01:33:51 -0400

    add a clear_logs() function

--- tests/test_mysql.py
+++ tests/test_mysql.py
@@ -1,5 +1,25 @@
 import datetime
+import pymysql
+import pytest
 from ..src import mysql
+
+
+# the number of events that will show up after clear_logs()
+BASELINE_NUM_EVENTS = 2
+
+
+@pytest.fixture
+def conn():
+    conn = pymysql.connect(host='mysql', db='mysql')
+    yield conn
+    conn.close()
+
+@pytest.fixture
+def db(conn):
+    db = mysql.MySQL(conn)
+    db.enable_logs()
+    db.clear_logs()
+    return db
 
 def test_mysql_to_cw_log_event():
     row = [
@@ -12,6 +32,15 @@ def test_mysql_to_cw_log_event():
         'timestamp': 1000000,
         'message': 'Query: SELECT * FROM sometable'
     }
+
+def test_clear_logs(conn, db):
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM general_log")
+        conn.commit()
+    assert db.num_log_events() == BASELINE_NUM_EVENTS + 2
+
+    db.clear_logs()
+    assert db.num_log_events() == BASELINE_NUM_EVENTS
 
 # TODO test_get_general_log_events
 
